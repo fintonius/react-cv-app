b6783e9bb3990e708dc4cd14b92a11c3eaee20c4
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-04-27 18:22:30 +0100

    add basic form data to input component

--- src/components/ProfileInput.jsx
+++ src/components/ProfileInput.jsx
@@ -1,5 +1,34 @@
 export default function ProfileInput() {
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log('help')
+        console.log(e)
+    }
+
     return (
-        <h2>Profile</h2>
+        
+        <div>
+            <h2>Profile</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="first-name">
+                    First Name
+                </label>
+                <input type="text" id="first-name" autoComplete="off" />
+                <label htmlFor="second-name">
+                   Second Name
+                </label>
+                <input type="text" id="second-name" autoComplete="off" />
+                <label htmlFor="email">
+                    Email
+                </label>
+                <input type="email" id="email" autoComplete="off" />
+                <label htmlFor="phone-number">
+                    Phone Number
+                </label>
+                <input type="number" id="phone-number" autoComplete="off" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
