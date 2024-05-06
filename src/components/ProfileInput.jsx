c88b5beeb9b0922ccb4362d5783c2c90ad3375d7
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-05-06 16:27:02 +0100

    can now pass user input from text fields in ProfileInput back to App

--- src/components/ProfileInput.jsx
+++ src/components/ProfileInput.jsx
@@ -1,51 +1,68 @@
-// this could end up being refactored as a single form component and used
-// for each input section which when added to the App file is sent a bunch 
-// of info as props that is then .map() into JSX?
 
 import { useState } from "react";
 
 export default function ProfileInput(props) {
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.addRecord('more hell scape');
-      }
 
-    function handleInput(e) {
-        console.log(e.target.value)
-    } 
+      const [name, setName] = useState('');
+      const [surName, setSurName] = useState('');
+      const [email, setEmail] = useState('');
+      const [phone, setPhone] = useState('');
     
-    const [formData, setFormData] = useState({
-        firstName: '',
-        surName: '',
-        email: '',
-    });
+      const onSubmit = (e) => {
+        e.preventDefault();
+        const profileData = {
+          name: name,
+          surName: surName,
+          email: email,
+          phone: phone
+        };
+        props.addInfo(profileData);
+      };
     
     return (        
         <div>
             <h2>Profile</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={onSubmit}>
                 <label htmlFor="first-name">
                     First Name
                 </label>
                 <input 
                     type="text" 
                     id="first-name" 
-                    autoComplete="off"
-                    value={formData.firstName}
-                    onChange={handleInput}
+                    name="first-name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
                 />
                 <label htmlFor="second-name">
                    Second Name
                 </label>
-                <input type="text" id="second-name" autoComplete="off" />
+                <input 
+                    type="text" 
+                    id="second-name" 
+                    value={surName}
+                    onChange={(e) => setSurName(e.target.value)}
+                    autoComplete="off" />
                 <label htmlFor="email">
                     Email
                 </label>
-                <input type="email" id="email" autoComplete="off" />
+                <input 
+                    type="email" 
+                    id="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    autoComplete="off" />
                 <label htmlFor="phone-number">
                     Phone Number
                 </label>
-                <input type="number" id="phone-number" autoComplete="off" />
+                <input 
+                    type="number" 
+                    id="phone-number"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    placeholder="Phone" 
+                    autoComplete="off" />
                 <button type="submit">Submit</button>
             </form>
         </div>
